adc10fb54a66c6aacb2a699f6755d6ee6fe2e534
SarahBechi <59558883+SarahBechi@users.noreply.github.com>
2020-03-23 16:45:41 +0100

    Updates

---
+++ precache-manifest.8e73ac61e23ad33ec666ff300280479d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e023068637a3bcbcfd60738bd4bb2e6",
+    "url": "/site-miss-confitures/index.html"
+  },
+  {
+    "revision": "475ae10a8325d1f87f61",
+    "url": "/site-miss-confitures/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "040a3c176c89f50dc798",
+    "url": "/site-miss-confitures/static/css/main.8ff1bd13.chunk.css"
+  },
+  {
+    "revision": "475ae10a8325d1f87f61",
+    "url": "/site-miss-confitures/static/js/2.c3d8eb19.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/site-miss-confitures/static/js/2.c3d8eb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040a3c176c89f50dc798",
+    "url": "/site-miss-confitures/static/js/main.dbe2729d.chunk.js"
+  },
+  {
+    "revision": "72343b79b6968cb3a8c6",
+    "url": "/site-miss-confitures/static/js/runtime-main.33e5c171.js"
+  }
+]);
